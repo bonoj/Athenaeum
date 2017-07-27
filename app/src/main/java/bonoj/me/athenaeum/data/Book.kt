3ca38b8128f3d569c69c62183187bcbd1ad63a28
bonoj <johnlbono@gmail.com>
2017-07-27 16:40:25 -0500

    refactor: Add Book secondary constructor

--- app/src/main/java/bonoj/me/athenaeum/data/Book.kt
+++ app/src/main/java/bonoj/me/athenaeum/data/Book.kt
@@ -1,10 +1,22 @@
 package bonoj.me.athenaeum.data
 
+import bonoj.me.athenaeum.data.model.ImageLinks
 import io.mironov.smuggler.AutoParcelable
 
 data class Book(
         val id: String,
         val title: String,
         val imageUrl: String
-) : AutoParcelable
+) : AutoParcelable {
+    constructor(id: String,
+                title: String,
+                authorString: String,
+                publisherString: String,
+                pageString: String,
+                categoriesString: String,
+                ratingsString: String,
+                description: String,
+                imageLinks: ImageLinks,
+                previewLink: String) : this(id, title, imageLinks.thumbnail)
+}
 // Parcelable implementation handled by Smuggler, no need to provide CREATOR field
